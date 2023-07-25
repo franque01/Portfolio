974f6d7168408e4810f1ddee325ddbf1033e6f28
Frank Okeke <franquechuks@gmail.com>
2023-07-25 14:14:49 +0100

    js apply

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const closeBtn = document.querySelector('#close');
 const menuBtn =  document.querySelector('.menubar');
 const menuBar = document.getElementById('menu');
+const navLinks = document.querySelectorAll('.headlinks')
 
 closeBtn.addEventListener('click', () => {
     menuBar.style.right = '-100%';
@@ -8,3 +9,12 @@ closeBtn.addEventListener('click', () => {
 menuBtn.addEventListener('click', () => {
     menuBar.style.right = '-2%';
 })
+
+for(let i = 0; i < navLinks.length; i++){
+    navLinks[i].addEventListener('click', () => {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    })
+    
+}
